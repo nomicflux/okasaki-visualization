138ce6b9833afb106f2832e3c668a00dfff3f54c
Michael A <nomicflux@gmail.com>
2016-10-04 20:22:35 -0500

    Added stack visualization

---
+++ src/Structures/Queue.elm
@@ -0,0 +1,32 @@
+module Structures.Queue exposing (..)
+
+import Maybe exposing (Maybe)
+
+import Structures.Stack as S
+
+type Queue a = Queue (S.Stack a) (S.Stack a)
+
+empty : Queue a
+empty = Queue (S.empty) (S.empty)
+
+isEmpty : Queue a -> Bool
+isEmpty (Queue left right) = (S.isEmpty left) && (S.isEmpty right)
+
+enqueue : a -> Queue a -> Queue a
+enqueue val (Queue left right) = Queue left (S.cons val right)
+
+top : Queue a -> Maybe a
+top (Queue left right) =
+    case left of
+        S.Cons val _ -> Just val
+        S.Nil -> case (S.reverse right) of
+                     S.Nil -> Nothing
+                     S.Cons val _ -> Just val
+
+dequeue : Queue a -> Maybe (Queue a)
+dequeue (Queue left right) =
+    case left of
+        S.Cons _ rst -> Just (Queue rst right)
+        S.Nil -> case (S.reverse right) of
+                     S.Nil -> Nothing
+                     S.Cons _ rst -> Just (Queue rst S.empty)
