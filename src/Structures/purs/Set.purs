e812f0e410787b763a06b739fcf15e7b326e7d56
Michael A <nomicflux@gmail.com>
2016-10-22 15:29:07 -0500

    Added Sets

--- src/Structures/purs/Set.purs
+++ src/Structures/purs/Set.purs
@@ -2,21 +2,30 @@ module Structures.Purs.Set where
 
 import Data.Foldable (class Foldable, foldr, foldl, foldMap)
 import Data.Functor (class Functor, map)
+import Data.Maybe (Maybe(..))
 import Data.Monoid (mempty)
 import Data.Ord (class Ord, compare, Ordering(..))
 import Data.Semigroup (append)
+import Data.Show (class Show, show)
+import Data.Tuple (Tuple(..), fst, snd)
+import Prelude (($), (+), (<>), (*), (-), max)
 
 import Structures.Purs.Stack as S
 
+-- | *Set
 data Set a = Leaf
            | Node { left :: Set a
                   , value :: a
                   , right :: Set a
                   }
+-- .end
 
+-- | *empty
 empty :: forall a. Set a
 empty = Leaf
+-- .end
 
+-- | *insert
 insert :: forall a. Ord a => Set a -> a -> Set a
 insert Leaf val = Node { left : Leaf, value : val, right : Leaf }
 insert s@(Node set) val =
@@ -24,7 +33,28 @@ insert s@(Node set) val =
     EQ -> s
     LT -> Node (set { left = insert set.left val })
     GT -> Node (set { right = insert set.right val })
+-- .end
 
+insertWithParent :: forall a. Ord a => Set a -> a -> Tuple (Set a) (Maybe a)
+insertWithParent set val = go set val Nothing
+  where
+    go :: Set a -> a -> (Maybe a) -> Tuple (Set a) (Maybe a)
+    go Leaf v mparent = Tuple (Node { left : Leaf, value : v, right : Leaf}) mparent
+    go (Node s) v mparent =
+      case compare v s.value of
+        EQ -> Tuple (Node s) mparent
+        LT ->
+          let
+            left = go s.left v (Just s.value)
+          in
+           Tuple (Node (s { left = fst left })) (snd left)
+        GT ->
+          let
+            right = go s.right v (Just s.value)
+          in
+           Tuple (Node (s { right = fst right })) (snd right)
+
+-- | *member
 member :: forall a. Ord a => Set a -> a -> Boolean
 member Leaf _ = false
 member (Node set) val =
@@ -32,6 +62,23 @@ member (Node set) val =
     EQ -> true
     LT -> member set.left val
     GT -> member set.right val
+-- .end
+
+get :: forall a. Ord a => Set a -> a -> Maybe a
+get Leaf _ = Nothing
+get (Node set) val =
+  case compare val set.value of
+    EQ -> Just set.value
+    LT -> get set.left val
+    GT -> get set.right val
+
+update :: forall a. Ord a => Set a -> a -> Set a
+update Leaf _ = Leaf
+update (Node set) val =
+  case compare val set.value of
+    EQ -> Node $ set { value = val }
+    LT -> Node $ set { left = update set.left val }
+    GT -> Node $ set { right = update set.right val }
 
 toStack :: forall a. Set a -> S.Stack a
 toStack Leaf = S.Nil
@@ -42,6 +89,62 @@ toStack (Node set) =
   in
    append left (S.cons set.value right)
 
+depth :: forall a. Ord a => Set a -> Int
+depth Leaf = 0
+depth (Node set) =
+  let
+    leftDepth = depth set.left
+    rightDepth = depth set.right
+    maxDepth = max leftDepth rightDepth
+  in
+   maxDepth + 1
+
+width :: forall a. Ord a => Set a -> Int
+width Leaf = 0
+width (Node set) =
+  let
+    leftWidth = width set.left
+    rightWidth = width set.right
+  in
+   1 + leftWidth + rightWidth
+
+fan :: forall a. Ord a => Set a -> Int
+fan Leaf = 0
+fan (Node set) =
+  let
+    leftFan = width set.left
+    rightFan = width set.right
+  in
+   1 + 2 * max leftFan rightFan
+
+leftWidth :: forall a. Ord a => Set a -> Int
+leftWidth Leaf = 0
+leftWidth (Node node) =
+  let
+    left = leftWidth node.left + 1
+    right = leftWidth node.right - 1
+  in
+   max left right
+
+rightWidth :: forall a. Ord a => Set a -> Int
+rightWidth Leaf = 0
+rightWidth (Node node) =
+  let
+    left = rightWidth node.left - 1
+    right = rightWidth node.right + 1
+  in
+   max left right
+
+leftMaxWidth :: forall a. Ord a => Set a -> Int
+leftMaxWidth Leaf = 0
+leftMaxWidth (Node node) = maxWidth node.left
+
+rightMaxWidth :: forall a. Ord a => Set a -> Int
+rightMaxWidth Leaf = 0
+rightMaxWidth (Node node) = maxWidth node.right
+
+maxWidth :: forall a. Ord a => Set a -> Int
+maxWidth node = 1 + 2 * max (leftWidth node) (rightWidth node)
 
 instance functorSet :: Functor Set where
   map _ Leaf = Leaf
@@ -61,3 +164,7 @@ instance foldableSet :: Foldable Set where
       right = foldMap f set.right
     in
      append (f set.value) (append left right)
+
+instance showSet :: Show a => Show (Set a) where
+  show Leaf = "."
+  show (Node set) = show set.left <> " / " <> show set.value <> " \\ " <> show set.right
---
+++ src/Views/Set.purs
@@ -0,0 +1,399 @@
+module Views.Set where
+
+import CodeSnippet as CS
+import Data.Map as M
+import Pux.Html as H
+import Pux.Html.Attributes as HA
+import Pux.Html.Events as HE
+import Structures.Purs.Set as S
+import Control.Monad.Eff.Class (liftEff)
+import Control.Monad.Eff.Exception (Error)
+import Data.Array (foldM, (:), concatMap, fromFoldable)
+import Data.Either (Either(..))
+import Data.Eq (class Eq, eq)
+import Data.Filterable (filterMap)
+import Data.Foldable (class Foldable, foldr)
+import Data.Function.Uncurried (runFn3)
+import Data.Functor (map, class Functor)
+import Data.Int (fromString, toNumber, round)
+import Data.Maybe (Maybe(..), fromMaybe)
+import Data.Ord (class Ord, compare)
+import Data.Show (show)
+import Data.Tuple (Tuple(..), fst, snd)
+import Debug.Trace (spy)
+import Math (atan, sin, cos, pi, pow)
+import Prelude (($), (+), (/), (-), (*), (<), (<>), (<<<), const, min, max, (<$>), bind, pure, negate)
+import Pux (EffModel, noEffects)
+import Pux.Html.Attributes (offset)
+import Signal ((~>))
+import Signal.Time (now, Time, millisecond)
+
+type Classes = String
+type NodeID = Int
+type NodeValue = Int
+data Node = Node { value :: NodeValue
+                 , classes :: Classes
+                 , id :: NodeID
+                 , connections :: Array NodeID
+                 }
+
+type NodePos = { x :: Number
+               , y :: Number
+               , r :: Number
+               , connections :: Array NodeID
+               , value :: NodeValue
+               , classes :: Classes
+               }
+type NodeMap = M.Map NodeID NodePos
+
+instance eqNode :: Eq Node where
+  eq (Node a) (Node b) = eq a.value b.value
+
+instance ordNode :: Ord Node where
+  compare (Node a) (Node b) = compare a.value b.value
+
+type Model = { set :: S.Set Node
+             , currId :: NodeID
+             , currInput :: Maybe NodeValue
+             , currNodes :: NodeMap
+             , prevNodes :: NodeMap
+             , startAnimation :: Maybe Time
+             , animationPhase :: Number
+             , delay :: Number
+             , sourceCode :: M.Map String String
+             , currFnName :: Maybe String
+             , currFn :: Maybe String
+             }
+
+initModel :: Model
+initModel = { set : S.empty
+            , currId : 0
+            , currInput : Nothing
+            , currNodes : M.empty
+            , prevNodes : M.empty
+            , startAnimation : Nothing
+            , animationPhase : 1.0
+            , delay : 750.0 * millisecond
+            , sourceCode : M.empty
+            , currFnName : Nothing
+            , currFn : Nothing
+            }
+
+mkNode :: Model -> Maybe (Tuple Node Model)
+mkNode model =
+  case model.currInput of
+    Nothing -> Nothing
+    Just val ->
+      let
+        newNode = Node { value : val
+                       , classes : "cons"
+                       , id : model.currId
+                       , connections : []
+                       }
+        newModel = model { currId = model.currId + 1 }
+      in
+       Just $ Tuple newNode newModel
+
+maxWidth :: Number
+maxWidth = 800.0
+
+maxHeight :: Number
+maxHeight = 600.0
+
+maxRadius :: Number
+maxRadius = 40.0
+
+buffer :: Number
+buffer = 10.0
+
+type Dimensions = { width :: Int
+                  , depth :: Int
+                  }
+type Position = { x :: Int
+                , y :: Int
+                }
+
+mkNodePos :: Dimensions -> Position -> Node -> NodePos
+mkNodePos dim pos (Node node) =
+  let
+    fwidth = toNumber (dim.width + 1)
+    fheight = toNumber (dim.depth + 1)
+    ftotal = max fwidth fheight
+    rY = maxWidth / 2.0 / (pow 2.0 (toNumber pos.y))
+    rH = maxWidth / 2.0 / (max fwidth fheight)
+    r = min maxRadius (min rY rH)
+    offset x = x + buffer + r
+    calcPosY y = offset $ (y * maxHeight / fheight)
+    calcPosX x y =
+      let
+        bins = pow 2.0 y
+        binSize = maxWidth / bins
+        binOffset = binSize / 2.0
+      in
+       x * binSize + binOffset
+    circPos = { x : calcPosX (toNumber pos.x) (toNumber pos.y)
+              , y : calcPosY (toNumber pos.y)
+              , r : r
+              , value : node.value
+              , connections : node.connections
+              , classes : node.classes
+              }
+  in
+   circPos
+
+getID :: Node -> NodeID
+getID (Node n) = n.id
+
+getNodeMap :: Dimensions -> S.Set Node -> NodeMap
+getNodeMap dim set = go set {x: 0, y: 0}
+  where
+    go :: S.Set Node -> Position -> NodeMap
+    go S.Leaf _ = M.empty
+    go (S.Node s) pos =
+      let
+        left = go s.left (pos {x = 2*pos.x, y = pos.y + 1})
+        right = go s.right (pos { x = 2*pos.x + 1, y = pos.y + 1})
+        combined = M.union left right
+        curr = mkNodePos dim pos s.value
+      in
+       M.insert (getID s.value) curr combined
+
+changeClass :: Classes -> Node -> Node
+changeClass classes (Node node) = Node (node { classes = classes })
+
+changeAllClasses :: forall f. Functor f => Classes -> f Node -> f Node
+changeAllClasses classes = map (changeClass classes)
+
+wipeClasses :: forall f. Functor f => f Node -> f Node
+wipeClasses = changeAllClasses ""
+
+data Action = Empty
+            | Member
+            | Insert
+            | CurrentInput String
+            | ShowStructure
+            | StartTimer Time
+            | LoadCode (Either String CS.SourceCode)
+            | Failure Error
+            | Tick Time
+
+changeFn :: Model -> String -> Model
+changeFn model fn = model { currFn = M.lookup fn model.sourceCode
+                          , currFnName = Just fn}
+
+updateSet :: Model -> S.Set Node -> String -> EffModel Model Action _
+updateSet model set fn =
+  let
+    depth = S.depth set
+    width = S.maxWidth set
+    -- leftWidth = S.leftMaxWidth set
+    -- rightWidth = S.rightMaxWidth set
+    newMap = getNodeMap {width : width, depth : depth} set
+  in
+   { state: model { prevNodes = model.currNodes
+                  , currNodes = newMap
+                  , set = set
+                  , animationPhase = 0.0
+                  , currFnName = Just fn
+                  , currFn = M.lookup fn model.sourceCode
+                  }
+   , effects: [ do
+      time <- liftEff now
+      pure $ StartTimer time
+              ]
+   }
+
+changeConn :: Node -> Array NodeID -> Node
+changeConn (Node node) conns = Node (node {connections = conns})
+
+blankNode :: NodeValue -> Node
+blankNode val = Node { value : val
+                     , classes : ""
+                     , id : -1
+                     , connections : []
+                     }
+
+changeClasses :: Node -> Classes -> Node
+changeClasses (Node node) classes = Node $ node { classes = classes }
+
+update :: Action -> Model -> EffModel Model Action _
+update (Failure err) model = noEffects model
+update (LoadCode (Left err)) model = noEffects model
+update (LoadCode (Right code)) model =
+  let
+    sourceCodeModel = model { sourceCode = CS.parseFunctions code }
+    newModel =
+      case model.currFnName of
+        Nothing -> sourceCodeModel
+        Just fn -> changeFn sourceCodeModel fn
+  in
+   noEffects newModel
+update (Tick time) model =
+  case model.startAnimation of
+    Nothing -> noEffects model
+    Just start ->
+      let
+        timeDiff = time - start
+      in
+       if model.delay < timeDiff
+       then noEffects $ model { animationPhase = 1.0 }
+       else noEffects $ model { animationPhase = sin (timeDiff / model.delay * pi / 2.0) }
+update (StartTimer time) model = noEffects $ model { startAnimation = Just time
+                                                   , animationPhase = 0.0
+                                                   }
+update Empty model =
+  updateSet model S.empty "empty"
+update Member model =
+  case model.currInput of
+    Nothing ->
+      updateSet model (wipeClasses model.set) "member"
+    Just val ->
+      let
+        node = S.get model.set (blankNode val)
+      in
+       case node of
+         Nothing ->
+           updateSet model (wipeClasses model.set) "member"
+         Just oldNode ->
+           updateSet model (S.update (wipeClasses model.set) $ changeClasses oldNode "head") "member"
+update Insert model =
+  let
+    cleanSet = wipeClasses model.set
+    mnode = mkNode model
+  in
+   case mnode of
+     Nothing -> noEffects $ changeFn model "insert"
+     Just (Tuple node newModel) ->
+       let
+         midSet = S.insertWithParent cleanSet node
+         finalSet = case snd midSet of
+           Nothing -> fst midSet
+           Just parent ->
+             S.update (fst midSet) (changeConn node [getID parent])
+       in
+        updateSet newModel finalSet "insert"
+update (CurrentInput s) model =
+  noEffects $ model { currInput = fromString s }
+update ShowStructure model =
+  noEffects $ changeFn model "Set"
+
+svgText :: forall a. Array (H.Attribute a) -> Array (H.Html a) -> H.Html a
+svgText = runFn3 H.element "text"
+
+initialNode :: NodeID -> NodePos
+initialNode nid = { x : maxWidth + 10.0
+                  , y : -10.0
+                  , r : 0.0
+                  , connections : [nid]
+                  , value : 0
+                  , classes : ""
+                  }
+
+finalNode :: NodeID -> NodePos
+finalNode _ = { x : -10.0
+              , y : maxHeight + 10.0
+              , r : 0.0
+              , connections : []
+              , value : 0
+              , classes : ""
+              }
+
+interpolate :: Number -> Number -> Number -> Number
+interpolate phase old new =
+  let
+    diff = new - old
+  in
+   old + phase*diff
+
+interpolateNodes :: Number -> NodePos -> NodePos -> NodePos
+interpolateNodes phase old new =
+  let
+    intphase = interpolate phase
+  in
+   new { x = intphase old.x new.x
+       , y = intphase old.y new.y
+       , r = intphase old.r new.r
+       }
+
+viewNodePos :: Number -> NodeMap -> NodeMap -> NodeID -> Array (H.Html Action)
+viewNodePos phase prevmap currmap nid =
+  let
+    interphase = interpolateNodes phase
+    prev = fromMaybe (initialNode nid) (M.lookup nid prevmap)
+    curr = fromMaybe (finalNode nid) (M.lookup nid currmap)
+    node = interphase prev curr
+    circ = H.circle [ HA.cx (show $ node.x)
+                    , HA.cy (show $ node.y)
+                    , HA.r (show $ node.r)
+                    , HA.className (node.classes <> " node")
+                    ] [ ]
+    val = svgText [ HA.x (show $ node.x)
+                  , HA.y (show $ node.y + 6.0)
+                  , HA.textAnchor "middle" ] [ H.text (show node.value)]
+    edges = filterMap (\n ->
+                        let
+                          mnode = M.lookup n currmap
+                        in
+                         case mnode of
+                           Nothing -> Nothing
+                           Just currEdge ->
+                             let
+                               prevEdge = fromMaybe (initialNode n) (M.lookup n prevmap)
+                               edgeNode = interphase prevEdge currEdge
+                               signX = if node.x < edgeNode.x then 1.0 else -1.0
+                               signY = if node.y < edgeNode.y then signX else -1.0 * signX
+                               invtan = atan ((node.y - edgeNode.y) / (node.x - edgeNode.x))
+                             in
+                              Just $ H.line [ HA.x1 (show $ node.x + signX*cos invtan * node.r)
+                                            , HA.y1 (show $ node.y - signY*sin invtan * node.r)
+                                            , HA.x2 (show $ edgeNode.x - signX*cos invtan * edgeNode.r)
+                                            , HA.y2 (show $ edgeNode.y + signY*sin invtan * edgeNode.r)
+                                            , HA.className "edge"] [ ]) node.connections
+  in
+   circ : val : edges
+
+view :: Model -> H.Html Action
+view model =
+  let
+    keys = M.keys $ M.union model.prevNodes model.currNodes
+    showNodes = viewNodePos model.animationPhase model.prevNodes model.currNodes
+    nodes = concatMap showNodes (fromFoldable keys)
+    stackDiv = H.div [ HA.className "render" ] [ H.svg [HA.height (show maxHeight)
+                                                       , HA.width (show maxWidth)  ] nodes ]
+    dataBtn = H.div [ ] [ H.button [ HA.className "pure-button pure-button-warning"
+                                   , HE.onClick $ const ShowStructure
+                                   ] [ H.text "Set Structure" ]
+                        ]
+    emptyBtn = H.div [ ] [ H.button [ HA.className "pure-button"
+                                    , HE.onClick $ const Empty
+                                    ] [ H.text "Empty" ]
+                         ]
+    insertSpan = H.div [ ] [ H.span [ ] [ H.button [ HA.className "pure-button"
+                                                   , HE.onClick $ const Insert
+                                                   ] [ H.text "Insert"]
+                                        , H.button [ HA.className "pure-button"
+                                                   , HE.onClick $ const Member
+                                                   ] [ H.text "Member"]
+                                        , H.input [ HA.type_ "number"
+                                                  , HE.onChange $ \t -> CurrentInput t.target.value
+                                                  ] [ ]]
+                         ]
+    controlDiv = H.div [ HA.className "pure-u-1-2" ] [ dataBtn
+                                                     , emptyBtn
+                                                     , insertSpan
+                                                     ]
+    codeDiv = H.div [ HA.className "pure-u-1-2" ]
+                    [ H.code [ ]
+                      [ H.pre [ ]
+                        [ case model.currFn of
+                             Nothing ->
+                               H.i []
+                                   [ H.text "No implementation given or no function selected"]
+                             Just fn -> H.text fn ]
+                      ]
+                    ]
+  in
+   H.div [ HA.className "pure-g" ] [ stackDiv
+                                   , controlDiv
+                                   , codeDiv
+                                   ]
