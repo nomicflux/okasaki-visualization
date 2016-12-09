ef868250e6a147e5bac118e89cddc66cb1b96768
Michael A <nomicflux@gmail.com>
2016-12-09 16:24:04 -0600

    Added Scala queue

---
+++ src/Structures/scala/Queue.scala
@@ -0,0 +1,80 @@
+package okasaki
+
+// | *Queue empty
+object Queue
+//.end
+{
+  // | *empty
+  def empty = new Queue(Nil, Nil)
+  // .end
+}
+
+// | *Queue isEmpty rotate top back pop push inject eject
+class Queue[+A](front: Stack[A], back: Stack[A])
+// .end
+{
+  // | *isEmpty
+  def isEmpty: Boolean =
+    front.isEmpty && back.isEmpty
+  // .end
+
+  // | *rotate
+  def rotate: Queue[A] =
+    new Queue(back.reverse, front.reverse)
+  // .end
+
+  // | *top
+  def top: A = front match {
+    case Cons(h, _) => h
+    case Nil => back.reverse match {
+      case Cons(h, _) => h
+      case Nil => throw new NoSuchElementException("Empty top")
+    }
+  }
+  // .end
+
+  // | *back
+  def bottom: A = back match {
+    case Cons(h, _) => h
+    case Nil => front.reverse match {
+      case Cons(h, _) => h
+      case Nil => throw new NoSuchElementException("Empty back")
+    }
+  }
+  // .end
+
+  // | *pop
+  def pop: Queue[A] = front match {
+    case Cons(_, t) => new Queue(t, back)
+    case Nil => back.reverse match {
+      case Cons(_, t) => new Queue(t, Nil)
+      case Nil => throw new NoSuchElementException("Empty pop")
+    }
+  }
+  // .end
+
+  // | *eject
+  def eject: Queue[A] = back match {
+    case Cons(_, t) => new Queue(front, t)
+    case Nil => front.reverse match {
+      case Cons(_, t) => new Queue(Nil, t)
+      case Nil => throw new NoSuchElementException("Empty eject")
+    }
+  }
+  // .end
+
+  // | *push
+  def push[B >: A](elem: B): Queue[B] =
+    new Queue(front.cons(elem), back)
+  // .end
+
+  // | *inject
+  def inject[B >: A](elem: B): Queue[B] =
+    new Queue(front, back.cons(elem))
+  // .end
+
+  def shift[B >: A](elem: B): Queue[B] = inject(elem)
+  def unshift: Queue[A] = pop
+
+  override def toString = front.toString ++ " // " ++ back.toString
+}
--- src/Structures/scala/Stack.scala
+++ src/Structures/scala/Stack.scala
@@ -1,5 +1,7 @@
+package okasaki
+
 // | *Stack head tail reverse cons
-abstract class Stack[A]
+abstract class Stack[+A]
 // .end
 {
   // | *head
@@ -12,12 +14,15 @@ abstract class Stack[A]
   def reverse: Stack[A]
   // .end
   // | *cons
-  def cons(x : A) = Cons(x, this)
+  def cons[B >: A](x : B): Stack[B] = Cons(x, this)
+  // .end
+  // | *isEmpty
+  def isEmpty: Boolean
   // .end
 }
 
 // | *Stack empty head tail reverse
-case class Nil[Nothing]() extends Stack[Nothing]
+case object Nil extends Stack[Nothing]
 // .end
 {
   // | *head
@@ -29,6 +34,10 @@ case class Nil[Nothing]() extends Stack[Nothing]
   // | *reverse
   def reverse = this
   // .end
+  // | *isEmpty
+  def isEmpty = true
+  // .end
+  override def toString = "[]"
 }
 
 // | *Stack head tail reverse
@@ -38,10 +47,15 @@ case class Cons[A](head: A, tail: Stack[A]) extends Stack[A]
   // | *reverse
   def reverse = {
     def reverseHelp(curr: Stack[A], acc: Stack[A]): Stack[A] = curr match {
-      case Nil() => acc
+      case Nil => acc
       case Cons(h, t) => reverseHelp(t, Cons(h, acc))
     }
-    reverseHelp(this, Nil())
+    reverseHelp(this, Nil)
   }
   // .end
+  // | *isEmpty
+  def isEmpty = false
+  // .end
+
+  override def toString = head.toString ++ " : " ++ tail.toString
 }
