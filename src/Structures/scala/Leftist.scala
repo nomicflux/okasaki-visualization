f3e03b40f4bbd61f816bcf6cc499cae73c43a5cb
Michael A <nomicflux@gmail.com>
2016-12-13 14:25:28 -0600

    Made companion objet

--- src/Structures/scala/Leftist.scala
+++ src/Structures/scala/Leftist.scala
@@ -1,28 +1,48 @@
 package okasaki.leftist;
 
-// | *LeftistHeap empty merge insert deleteMin findMin
-sealed trait Leftist[+A <% Ordered[A]]
-// .end
+object Leftist
 {
   // | *merge insert deleteMin
-  private def singleton[A <% Ordered[A]](value: A): Leftist[A] =
+  // Static
+  def singleton[A <% Ordered[A]](value: A): Leftist[A] =
     new Node(Leaf, value, Leaf, 1)
   // .end
 
   // | *merge insert deleteMin
+  // Static
+  def makeNode[A <% Ordered[A]](newVal: A,
+                                        aHeap: Leftist[A],
+                                        bHeap: Leftist[A]): Leftist[A] =
+    if(aHeap.rank >= bHeap.rank)
+      new Node(aHeap, newVal, bHeap, bHeap.rank + 1)
+    else
+      new Node(bHeap, newVal, aHeap, aHeap.rank + 1)
+  // .end
+}
+
+// | *LeftistHeap empty merge insert deleteMin findMin
+sealed abstract class Leftist[+A <% Ordered[A]]
+// .end
+{
+  // | *merge insert deleteMin
+  // Class
   def merge[B >: A <% Ordered[B]](other: Leftist[B]): Leftist[B]
   // .end
   // | *insert
+  // Class
   def insert[B >: A <% Ordered[B]](value: B): Leftist[B] =
-    this.merge(singleton(value))
+    this.merge(Leftist.singleton(value))
   // .end
   // | *deleteMin
+  // Class
   def deleteMin: Leftist[A]
   // .end
   // | *findMin
+  // Class
   def findMin: A
   // .end
   // | *rank
+  // Class
   def rank: Int
   // .end
 }
@@ -32,15 +52,19 @@ case object Leaf extends Leftist[Nothing]
 // .end
 {
   // | *deleteMin
+  // Leaf
   def deleteMin = throw new NoSuchElementException("Leaf.deleteMin")
   // .end
   // | *findMin
+  // Leaf
   def findMin = throw new NoSuchElementException("Leaf.findMin")
   // .end
   // | *merge insert deleteMin
+  // Leaf
   def merge[A <% Ordered[A]](other: Leftist[A]): Leftist[A] = other
   // .end
   // | *rank
+  // Leaf
   def rank = 0
   // .end
 }
@@ -52,31 +76,24 @@ case class Node[A <% Ordered[A]](left: Leftist[A],
                                  rank: Int) extends Leftist[A]
 // .end
 {
-  // | *merge insert deleteMin
-  private def makeNode[A <% Ordered[A]](newVal: A,
-                                 aHeap: Leftist[A],
-                                 bHeap: Leftist[A]): Leftist[A] =
-   if(aHeap.rank >= bHeap.rank)
-     new Node(aHeap, newVal, bHeap, bHeap.rank + 1)
-   else
-     new Node(bHeap, newVal, aHeap, aHeap.rank + 1)
-  // .end
-
   // | *findMin
+  // Node
   def findMin: A = value
   // .end
   // | *deleteMin
+  // Node
   def deleteMin: Leftist[A] = left.merge(right)
   // .end
   // | *merge insert deleteMin
+  // Node
   def merge[B >: A <% Ordered[B]](other: Leftist[B]): Leftist[B] =
     other match {
       case Leaf => this
       case Node(l,v,r,n) =>
         if(value <= v)
-          makeNode(value, left, right.merge(other))
+          Leftist.makeNode(value, left, right.merge(other))
         else
-          makeNode(v, l, this.merge(r))
+          Leftist.makeNode(v, l, this.merge(r))
     }
   // .end
 }
